6e03faffec81c7af7e3c995425f6e8c7c19d5b54
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2018-10-16 14:24:22 +0300

    Add build process, configs and some components from other modules

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM node:8-alpine
+RUN apk update
+
+RUN apk add --update bash
+
+RUN apk add git
+RUN npm i -g npm@5.6.0
+RUN npm install -g --unsafe-perm bower polymer-cli gulp-cli
+
+
+WORKDIR /tmp
+ADD bower.json /tmp/
+ADD package.json /tmp/
+
+RUN npm install
+RUN bower --allow-root install
+
+RUN mkdir /code/
+ADD . /code/
+
+# remove installed modules for clean setup
+RUN rm -rf /code/build/
+RUN rm -rf /code/node_modules/
+RUN rm -rf /code/bower_modules/
+
+WORKDIR /code
+RUN mv /tmp/node_modules /code/node_modules
+RUN mv /tmp/bower_components /code/bower_components
+RUN gulp
+EXPOSE 8080
+CMD ["node", "express.js"]
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+
+const clean = require('./gulp-tasks/clean');
+const path = require('path');
+const preBuild = require('./gulp-tasks/pre-build');
+const postBuild = require('./gulp-tasks/post-build');
+const buildElements = require('./gulp-tasks/build-elements');
+const copyAssets = require('./gulp-tasks/copy-assets');
+const copyImages = require('./gulp-tasks/copy-images');
+const copyBower = require('./gulp-tasks/copy-bower');
+const runTests = require('./gulp-tasks/test');
+const jsLinter = require('./gulp-tasks/js-linter');
+
+global.config = {
+    appName: 'etoolsApd',
+    polymerJsonPath: path.join(process.cwd(), 'polymer.json'),
+    buildDirectory: 'build'
+};
+
+const build = require('./gulp-tasks/build');
+
+gulp.task('watch', function() {
+    gulp.watch(['./src/elements/**/*.*'], gulp.series(jsLinter, buildElements));
+    gulp.watch(['./src/manifest.json', './index.html'], gulp.series(copyAssets));
+    gulp.watch(['./src/images/**/*.*'], gulp.series(copyImages));
+    gulp.watch(['./src/bower_components/**/*.*'], gulp.series(copyBower()));
+});
+
+gulp.task('lint', gulp.series(jsLinter));
+gulp.task('test', gulp.series(clean, gulp.parallel(buildElements, copyAssets, copyBower()), runTests));
+
+gulp.task('startServer', () => {nodemon({script: 'express.js'});});
+
+gulp.task('devBuild', gulp.series(clean, jsLinter, gulp.parallel(buildElements, copyAssets, copyImages, copyBower())));
+gulp.task('prodBuild', gulp.series(clean, preBuild, build, postBuild));
+
+gulp.task('devup', gulp.series('devBuild', gulp.parallel('startServer', 'watch')));
+
+gulp.task('default', gulp.series(['prodBuild']));
