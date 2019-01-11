9141dd453ac88c6380d71ff54f7beaa30371923b
Adrian Hangan <iovuadrian@gmail.com>
2019-01-11 10:27:29 +0200

    delete generated *.js files

--- src/components/utils/fire-custom-event.js
+++
@@ -1,7 +0,0 @@
-export const fireEvent = (el, eventName, eventDetail) => {
-    el.dispatchEvent(new CustomEvent(eventName, {
-        detail: eventDetail,
-        bubbles: true,
-        composed: true
-    }));
-};
