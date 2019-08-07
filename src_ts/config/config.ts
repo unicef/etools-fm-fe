a5eb3a5a21f0e0767494c5424484c36a5dc82966
Roman Karpovich <fpm.th13f@gmail.com>
2019-08-07 17:24:28 +0300

    circleci with dockerfiles

--- src_ts/config/config.ts
+++ src_ts/config/config.ts
@@ -4,7 +4,7 @@ const DEV_DOMAIN: string = 'etools-dev.unicef.org';
 const DEMO_DOMAIN: string = 'etools-demo.unicef.org';
 
 export const SMALL_MENU_ACTIVE_LOCALSTORAGE_KEY: string = 'etoolsAppSmallMenuIsActive';
-export const ROOT_PATH: string = window.location.port !== '8080' ? '/fm/' : '/';
+export const ROOT_PATH: string = '/fm/';
 
 export function isProductionServer(): boolean {
     const location: string = window.location.href;
