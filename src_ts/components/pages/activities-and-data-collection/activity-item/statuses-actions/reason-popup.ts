8f051abd2e255ee357b25f457258da83b1e7b2e1
AntonKrotevitch <anton.k@razortheory.com>
2020-03-25 12:36:03 +0300

    Textarea maxRows helper added

--- src_ts/components/pages/activities-and-data-collection/activity-item/statuses-actions/reason-popup.ts
+++ src_ts/components/pages/activities-and-data-collection/activity-item/statuses-actions/reason-popup.ts
@@ -1,13 +1,25 @@
-import {css, CSSResultArray, customElement, html, LitElement, property, TemplateResult} from 'lit-element';
+import {
+  CSSResultArray,
+  customElement,
+  html,
+  LitElement,
+  property,
+  PropertyValues,
+  query,
+  TemplateResult
+} from 'lit-element';
 import {fireEvent} from '../../../../utils/fire-custom-event';
 import {SharedStyles} from '../../../../styles/shared-styles';
 import {InputStyles} from '../../../../styles/input-styles';
 import {DialogStyles} from '../../../../styles/dialog-styles';
 import '@polymer/paper-input/paper-textarea';
 import {get, translate} from 'lit-translate';
+import {setTextareasMaxHeight} from '../../../../utils/textarea-max-rows-helper';
+import {PaperTextareaElement} from '@polymer/paper-input/paper-textarea';
 
 @customElement('reason-popup')
 export class ChecklistAttachments extends LitElement {
+  @query('#details-input') textarea!: PaperTextareaElement;
   @property() protected dialogOpened: boolean = true;
   @property() protected popupTitle: string | Callback = '';
   @property() protected label: string | Callback = '';
@@ -33,19 +45,18 @@ export class ChecklistAttachments extends LitElement {
         @close="${this.onClose}"
         @confirm-btn-clicked="${() => this.confirmReason()}"
       >
-        <div class="reason-container">
-          <paper-textarea
-            id="details-input"
-            .value="${this.reason}"
-            required
-            label="${translate(this.label as string)}"
-            placeholder="${get('MAIN.ENTER') + ` ${get(this.label as string)}`}"
-            @value-changed="${({detail}: CustomEvent) => (this.reason = detail.value)}"
-            @focus="${() => (this.error = '')}"
-            ?invalid="${Boolean(this.error)}"
-            error-message="${this.error}"
-          ></paper-textarea>
-        </div>
+        <paper-textarea
+          id="details-input"
+          .value="${this.reason}"
+          required
+          label="${translate(this.label as string)}"
+          placeholder="${get('MAIN.ENTER') + ` ${get(this.label as string)}`}"
+          @value-changed="${({detail}: CustomEvent) => (this.reason = detail.value)}"
+          @focus="${() => (this.error = '')}"
+          ?invalid="${Boolean(this.error)}"
+          error-message="${this.error}"
+          maxRows="3"
+        ></paper-textarea>
       </etools-dialog>
     `;
   }
@@ -62,16 +73,12 @@ export class ChecklistAttachments extends LitElement {
     fireEvent(this, 'response', {confirmed: true, response: {comment: this.reason}});
   }
 
+  protected firstUpdated(_changedProperties: PropertyValues): void {
+    super.firstUpdated(_changedProperties);
+    setTextareasMaxHeight(this.textarea);
+  }
+
   static get styles(): CSSResultArray {
-    return [
-      SharedStyles,
-      css`
-        .reason-container {
-          overflow-y: auto;
-          overflow-x: hidden;
-          max-height: 150px;
-        }
-      `
-    ];
+    return [SharedStyles];
   }
 }
