c0987a67b2bff41792c087406c134844203ad436
Mercea Emil <office@insightfeatures.com>
2021-12-07 16:44:52 +0200

    [ch27673] Correct label for Edit Points of Note

--- src_ts/components/pages/templates/issue-tracker-tab/issue-tracker-popup/issue-tracker-popup.tpl.ts
+++ src_ts/components/pages/templates/issue-tracker-tab/issue-tracker-popup/issue-tracker-popup.tpl.ts
@@ -30,7 +30,7 @@ export function template(this: IssueTrackerPopup): TemplateResult {
       ?opened="${this.dialogOpened}"
       .okBtnText="${translate(this.isNew ? 'MAIN.BUTTONS.ADD' : 'MAIN.BUTTONS.SAVE')}"
       .hideConfirmBtn="${this.isReadOnly}"
-      dialog-title="${translate('ISSUE_TRACKER.POPUP_TITLE')}"
+      dialog-title="${translate(this.isNew ? 'ISSUE_TRACKER.ADD_POPUP_TITLE' : 'ISSUE_TRACKER.EDIT_POPUP_TITLE')}"
       @iron-overlay-closed="${({target}: CustomEvent) => this.resetData(target)}"
       @confirm-btn-clicked="${() => this.processRequest()}"
       @close="${this.onClose}"
