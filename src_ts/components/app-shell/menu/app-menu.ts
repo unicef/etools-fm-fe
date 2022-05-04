2331b265b053c7adb8af3d0cf10b25102be216f7
Trif Adriana <trif.corina.adriana@gmail.com>
2022-05-04 18:07:15 +0300

    Implem. Intell link

--- src_ts/components/app-shell/menu/app-menu.ts
+++ src_ts/components/app-shell/menu/app-menu.ts
@@ -141,7 +141,7 @@ export class AppMenu extends MatomoMixin(LitElement) {
 
         <a
           class="nav-menu-item lighter-item"
-          href="https://app.powerbi.com/groups/me/apps/2c83563f-d6fc-4ade-9c10-bbca57ed1ece/reports/f9497d74-a670-4051-8800-a9d82284954f/ReportSection"
+          href="https://app.powerbi.com/groups/me/apps/2c83563f-d6fc-4ade-9c10-bbca57ed1ece/reports/9726e9e7-c72f-4153-9fd2-7b418a1e426c/ReportSection?ctid=77410195-14e1-4fb8-904b-ab1892023667"
           target="_blank"
           @tap="${this.trackAnalytics}"
           tracker="Implementation Intelligence"
