a28a943d39257b30b6e09cbf977b2d6275d46f8e
Trif Adriana <trif.corina.adriana@gmail.com>
2022-08-09 16:18:05 +0300

    Coverage tabs pagination

--- src_ts/components/pages/analyze/monitoring-tab/coverage/shared-tab-template.ts
+++ src_ts/components/pages/analyze/monitoring-tab/coverage/shared-tab-template.ts
@@ -2,6 +2,7 @@ import {css, CSSResult, customElement, html, LitElement, property, TemplateResul
 import '../../../../common/progressbar/column-item-progress-bar';
 import {repeat} from 'lit-html/directives/repeat';
 import {translate} from 'lit-translate';
+import PaginationMixin from '@unicef-polymer/etools-modules-common/dist/mixins/pagination-mixin';
 
 enum WidthCalculationTargets {
   DAYS = 'days',
@@ -10,10 +11,21 @@ enum WidthCalculationTargets {
 
 //TODO think about name
 @customElement('shared-tab-template')
-export class SharedTabTemplate extends LitElement {
-  @property() label!: string;
-  @property() data!: (InterventionsCoverage | CpOutputCoverage)[];
+export class SharedTabTemplate extends PaginationMixin(LitElement) {
+  @property() paginatedData!: (InterventionsCoverage | CpOutputCoverage)[];
   @property() loading = false;
+  @property() label!: string;
+  private _data!: (InterventionsCoverage | CpOutputCoverage)[];
+
+  @property()
+  get data(): (InterventionsCoverage | CpOutputCoverage)[] {
+    return this._data;
+  }
+
+  set data(val: (InterventionsCoverage | CpOutputCoverage)[]) {
+    this._data = val;
+    this.paginator = {...this.paginator, page: 1, page_size: 10, count: this._data.length};
+  }
 
   render(): TemplateResult {
     return html`
@@ -25,7 +37,7 @@ export class SharedTabTemplate extends LitElement {
         <!--   Tab content label   -->
         <label class="coverage-label">${this.label}</label>
         ${repeat<InterventionsCoverage | CpOutputCoverage>(
-          this.data,
+          this.paginatedData,
           (item: InterventionsCoverage | CpOutputCoverage) => item.id,
           (item: InterventionsCoverage | CpOutputCoverage) => html`
             <div class="progressbar-container">
@@ -54,6 +66,16 @@ export class SharedTabTemplate extends LitElement {
             </div>
           `
         )}
+        <etools-data-table-footer
+          .pageSize="${this.paginator.page_size}"
+          .pageNumber="${this.paginator.page}"
+          .totalResults="${this.paginator.count}"
+          .visibleRange="${this.paginator.visible_range}"
+          @visible-range-changed="${this.visibleRangeChanged}"
+          @page-size-changed="${this.pageSizeChanged}"
+          @page-number-changed="${this.pageNumberChanged}"
+        >
+        </etools-data-table-footer>
       </div>
     `;
   }
@@ -79,6 +101,17 @@ export class SharedTabTemplate extends LitElement {
     return item.days_since_visit && item.avg_days_between_visits && item.days_since_visit > item.avg_days_between_visits
       ? '#3AB78F'
       : '#FAED77';
+  }
+
+  _paginate(pageNumber: number, pageSize: number) {
+    if (!this.data) {
+      return;
+    }
+    this.paginatedData = (this.data || []).slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
+  }
+
+  paginatorChanged() {
+    this._paginate(this.paginator.page, this.paginator.page_size);
   }
 
   static get styles(): CSSResult {
