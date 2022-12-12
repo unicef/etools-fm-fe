5a055d773b3138fac3f424970ca314ad92190f72
dcioara <dan.cioara@nordlogic.com>
2022-12-12 21:40:24 +0200

    label position fix

--- src_ts/components/pages/activities-and-data-collection/activities-list/activities-list.tpl.ts
+++ src_ts/components/pages/activities-and-data-collection/activities-list/activities-list.tpl.ts
@@ -14,6 +14,7 @@ export function template(this: ActivitiesListComponent): TemplateResult {
         }
         --paper-input-container-label-floating: {
           font-size: 14px;
+          top: 0px;
         }
       }
     </style>
