74a09d74531dd4001984aa7622e1bf27046125bd
Mercea Emil <office@insightfeatures.com>
2023-11-14 15:59:10 +0200

    Fix dropdown styles in header

--- src_ts/components/common/layout/support-btn.ts
+++ src_ts/components/common/layout/support-btn.ts
@@ -1,5 +1,5 @@
 /* eslint-disable lit/attribute-value-entities */
-import {css, CSSResult, html, LitElement, TemplateResult} from 'lit';
+import {html, LitElement, TemplateResult} from 'lit';
 import {customElement} from 'lit/decorators.js';
 import '@unicef-polymer/etools-unicef/src/etools-icons/etools-icon';
 import {translate} from 'lit-translate';
@@ -12,34 +12,30 @@ import MatomoMixin from '@unicef-polymer/etools-piwik-analytics/matomo-mixin';
  */
 @customElement('support-btn')
 export class SupportBtn extends MatomoMixin(LitElement) {
-  static get styles(): CSSResult {
-    // language=CSS
-    return css`
-      :host(:hover) {
-        cursor: pointer;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-        font-size: 16px;
-      }
-
-      etools-icon {
-        margin-right: 4px;
-      }
-
-      @media (max-width: 650px) {
-        .support-text {
-          display: none;
-        }
-      }
-    `;
-  }
-
   render(): TemplateResult {
     // language=HTML
     return html`
+      <style>
+        :host(:hover) {
+          cursor: pointer;
+        }
+
+        a {
+          color: var(--light-secondary-text-color);
+          text-decoration: none;
+          font-size: 16px;
+        }
+
+        etools-icon {
+          margin-right: 4px;
+        }
+
+        @media (max-width: 650px) {
+          .support-text {
+            display: none;
+          }
+        }
+      </style>
       <a
         href="https://unicef.service-now.com/cc?id=sc_cat_item&sys_id=c8e43760db622450f65a2aea4b9619ad&sysparm_category=99c51053db0a6f40f65a2aea4b9619af"
         target="_blank"
