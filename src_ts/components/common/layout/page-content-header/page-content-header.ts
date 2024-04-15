7900e2c0a1f67278a92a09d531c513c6806e3569
Dan <dan.cioara@nordlogic.com>
2024-04-15 10:26:36 +0300

    style fix

--- src_ts/components/common/layout/page-content-header/page-content-header.ts
+++ src_ts/components/common/layout/page-content-header/page-content-header.ts
@@ -24,57 +24,6 @@ export class PageContentHeader extends LitElement {
           display: none !important;
         }
 
-      :host {
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        flex: 1;
-
-        background-color: var(--primary-background-color);
-        padding: 0 24px;
-        min-height: 85px;
-        border-bottom: 1px solid var(--dark-divider-color);
-      }
-
-      :host([with-tabs-visible]) {
-        min-height: 114px;
-      }
-
-      .content-header-row {
-        display: flex;
-        justify-content: flex-start;
-      }
-
-      .title-row {
-        display: flex;
-        align-items: center;
-        flex-wrap: nowrap;
-        margin: var(--table-row-margin, 30px 0 0);
-        padding: 0 24px;
-        min-height: var(--table-row-height, 36px);
-      }
-
-      .title-row h1 {
-        display: flex;
-        margin: var(--title-margin);
-        margin: 0;
-        font-weight: normal;
-        text-transform: capitalize;
-        font-size: var(--etools-font-size-24, 24px);
-        line-height: 1.3;
-        min-height: 31px;
-      }
-
-      .title-row > .title-row-actions {
-        flex: none;
-        margin-inline-start: auto;
-      }
-
-      .tabs {
-        margin-top: 5px;
-      }
-
-      @media print {
         :host {
           display: flex;
           flex-direction: column;
